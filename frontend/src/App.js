a37754a82114ec0047770e4a377523736d3c7271
syafikadin <syafik.adin222@gmail.com>
2023-08-27 22:57:19 +0700

    2. Header & Initial Pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Header from './pages/components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <>
+      <Router>
+        <div className='container'>
+          <Header />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+          </Routes>        
+        </div>
+      </Router>
+    </>
   );
 }
 
