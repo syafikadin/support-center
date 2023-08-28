d06944c2bc26595920a8523640f3eb9d40725bca
syafikadin <syafik.adin222@gmail.com>
2023-08-28 12:48:53 +0700

    7. Logout User

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,79 @@
+import { useState } from "react"
+import { FaSignInAlt } from 'react-icons/fa'
+import {useSelector, useDispatch} from 'react-redux'
+import {login} from '../features/auth/authSlice'
+
 function Login() {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  })
+
+  const {email, password} = formData
+
+  const dispatch = useDispatch()
+
+  const {user, isLoading, isSuccess, message} = useSelector(state => state.auth)
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }))
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    
+    const userData = {
+      email,
+      password
+    }
+
+    dispatch(login(userData))
+  }
+
     return (
-      <div>Login</div>
+      <>
+        <section className="heading">
+          <h1><FaSignInAlt /> Login</h1>
+          <p>Please log in to get support</p>
+        </section>
+
+        <section className="form">
+          <form onSubmit={onSubmit}>
+            <div className="form-group">
+              <input 
+                name="email" 
+                type="email" 
+                className="form-control" 
+                id="email" 
+                value={email} 
+                onChange={onChange} 
+                placeholder="Enter your email"
+                required
+              />
+            </div>
+            <div className="form-group">
+              <input 
+                name="password" 
+                type="password" 
+                className="form-control" 
+                id="password" 
+                value={password} 
+                onChange={onChange} 
+                placeholder="Enter your password"
+                required
+              />
+            </div>
+            <div className="form-group">
+              <button className="btn btn-block">
+                Submit
+              </button>
+            </div>
+          </form>
+        </section>
+      </>
     )
   }
   
